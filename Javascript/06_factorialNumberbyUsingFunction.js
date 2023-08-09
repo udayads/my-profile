2ec02c67ffc58f72a3a1bf794705d306e0b9d3ac
udayads <dsudaya8@gmail.com>
2023-08-09 10:02:26 +0530

    Factorial number using function
    Factorial number using function

---
+++ Javascript/06_factorialNumberbyUsingFunction.js
@@ -0,0 +1,12 @@
+
+function factNumber(num){
+    var result=1;
+    for (let i=num; i>0; i--) {
+        var total= result=result * i;
+    }
+    console.log(`The factorial of the ${num} is ${total}`);
+}
+factNumber(5);
+factNumber(9);
+factNumber(11);
+factNumber(7);
